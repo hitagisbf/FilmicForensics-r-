610119c1f0fbcbb4dd4d1df4c29b7a6f10444186
sshahirr <126746195+sshahirr@users.noreply.github.com>
2024-05-15 03:23:45 -0400

    Update Movie.js

--- Shahir/components/Movie.js
+++ Shahir/components/Movie.js
@@ -33,30 +33,32 @@ const TrendMovie = () => {
     <div style={{ backgroundColor: '#1E0D1E', padding: '1px' }}>
       <div className='sspheader'>Trending</div>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-      {movieList.map((movie, index) => (
-        <div key={index} style={{ position: 'relative', display: 'inline-block'}}>
-          <Link to={`/movie/${movie.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <img className='ssarray'
-              id={`trend_image_${index}`}            
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
-            />
-          </Link>
-          <div id={`rectangle_${index}`} className='ssinfo'>
-            <h2 style={{ color: 'white', marginTop: '15px', marginBottom: '0px' }}>{movie.title || movie.name} </h2>
-            <h3 style={{ color: 'white', marginTop: '0px', margin: '3px'}}>
-              {movie.vote_average ? `${String(movie.vote_average).substring(0, 3)}/10` : ''}
-            </h3>
-            <p style={{ color: 'white', marginTop: '5px' }}>
-              {movie.overview && movie.overview.length > 300 ? `${movie.overview.substring(0, 300)}...` : movie.overview}</p>
+        {movieList.map((media, index) => (
+          <div key={index} style={{ position: 'relative', display: 'inline-block'}}>
+            <Link to={`/${media.media_type === 'movie' ? 'movie' : 'tv'}/${media.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <img className='ssarray'
+                id={`trend_image_${index}`}            
+                src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
+                alt = "Movie Poster"
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave(index)}
+              />
+            </Link>
+            <div id={`rectangle_${index}`} className='ssinfo'>
+              <h2 style={{ color: 'white', marginTop: '15px', marginBottom: '0px' }}>{media.title || media.name} </h2>
+              <h3 style={{ color: 'white', marginTop: '0px', margin: '3px'}}>
+                {media.vote_average ? `${String(media.vote_average).substring(0, 3)}/10` : ''}
+              </h3>
+              <p style={{ color: 'white', marginTop: '5px' }}>
+                {media.overview && media.overview.length > 300 ? `${media.overview.substring(0, 300)}...` : media.overview}</p>
+            </div>
           </div>
+        ))}
         </div>
-      ))}
+        <p style={{ color: 'white' }}>Courtesy of TMDb</p>
       </div>
-      <p style={{ color: 'white' }}>Courtesy of TMDb</p>
-    </div>
-  );
-};
+    );
+  };
+  
 
 export default TrendMovie;
